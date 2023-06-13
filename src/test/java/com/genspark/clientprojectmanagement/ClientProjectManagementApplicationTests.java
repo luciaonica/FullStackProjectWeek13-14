01c4d2274a4705fc5b7f3b52277aadc078742640
luisgarcilazo <luis_david_garcilazo_medrano@hotmail.com>
2023-06-13 12:24:41 -0500

    initial files

---
+++ src/test/java/com/genspark/clientprojectmanagement/ClientProjectManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genspark.clientprojectmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientProjectManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
