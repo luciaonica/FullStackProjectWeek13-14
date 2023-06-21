89d2b1b08fe4911754193bfa0ed25c29623d876a
luisgarcilazo <luis_david_garcilazo_medrano@hotmail.com>
2023-06-21 10:06:10 -0500

    changed dashboard home

--- front-end/project-management/src/app/components/dashboard-home/dashboard-home.component.ts
+++ front-end/project-management/src/app/components/dashboard-home/dashboard-home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-dashboard-home',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class DashboardHomeComponent {
 
+  constructor(private authService:AuthService){}
+
+  showDevTab(){
+    return this.authService.isAuthenticatedDev();
+  }
 }
